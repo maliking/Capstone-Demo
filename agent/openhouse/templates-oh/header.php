5a7f3a9484fd7c7f52139f114507e5f9e41ec263
erinengle21 <eengle@csumb.edi>
2018-04-25 17:28:25 -0700

    changing header for create flyer page

--- agent/openhouse/templates-oh/header.php
+++ agent/openhouse/templates-oh/header.php
@@ -1,3 +1,149 @@
+<?php
+
+if ($_SESSION['userId'] == "100" || $_SESSION['userId'] == "34") //Jorge
+{
+    $_SESSION['userId'] = "34";
+    $_SESSION['userType'] = "1";
+}
+else if($_SESSION['userId'] == "101" || $_SESSION['userId'] == "37" ) //Juan
+{
+    $_SESSION['userId'] = "37";
+    $_SESSION['userType'] = "1";
+}
+$currAgent = "SELECT * FROM UsersInfo WHERE userId = '" . $_SESSION['userId'] . "'";
+$email = $dbConn->prepare($currAgent);
+$email->execute();
+$agentInfo = $email->fetch();
+
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+
+$sqlLicense = "SELECT license, mlsId FROM UsersInfo WHERE userId = :userId";
+
+$namedParameters = array();
+$namedParameters[':userId'] = $_SESSION['userId'];
+
+
+$licenseStmt = $dbConn->prepare($sqlLicense);
+$licenseStmt->execute($namedParameters);
+$licenseResult = $licenseStmt->fetch();
+
+$fyGrossSql = "SELECT TYGross FROM commInfo WHERE license = :license ORDER BY TYGross DESC LIMIT 1";
+$fyParam = array();
+$fyParam['license'] = $agentInfo['license'];
+
+$fyStmt = $dbConn->prepare($fyGrossSql);
+$fyStmt->execute($fyParam);
+$fyResult = $fyStmt->fetch();
+
+
+$activeListings = 0;
+$potentialGross = 0;
+for($i = 0; $i < sizeof($keys); $i++)
+{
+    if($response[$keys[$i]]['listingAgentID'] == $agentInfo['mlsId'])
+    {
+        $potentialGross += $response[$keys[$i]]['rntLsePrice'];
+        $activeListings++;
+    }
+}
+
+// $potentialGross = potentialAlgo($fyResult['TYGross'], $potentialGross) - (350 * $activeListings);
+
+$prevYearConn =getConnection();
+
+$prevYearGross = "SELECT SUM(finalComm) as prevGross, license FROM `commInfo` WHERE
+                YEAR(date) = YEAR(DATE_SUB( CURRENT_TIMESTAMP () , INTERVAL 1 YEAR )) AND license = :license GROUP BY license";
+$prevYearParameters = array();
+$prevYearParameters[':license'] = $agentInfo['license'];
+$prevYearStmt = $prevYearConn->prepare($prevYearGross);
+$prevYearStmt->execute($prevYearParameters);
+$prevYearResult = $prevYearStmt->fetch();
+
+$addedHouses = "SELECT count(*) as added FROM HouseInfo WHERE userId = :userId AND status = :status";
+$addedHouseParam = array();
+$addedHouseParam[':userId'] = $_SESSION['userId'];
+$addedHouseParam[':status'] = "added";
+
+$addedHousesStmt = $dbConn->prepare($addedHouses);
+$addedHousesStmt->execute($addedHouseParam);
+$addedHouseResults = $addedHousesStmt->fetch();
+
+
+
+
+$sql = "SELECT firstName, lastName, count(*) as sold, AVG(finalComm) as average, SUM(finalComm) AS earnings, AVG(percentage) AS avgPercent FROM commInfo WHERE license = :license GROUP BY license ";
+$parameters = array();
+$parameters[':license'] = $licenseResult['license'];
+
+$stmt = $dbConn->prepare($sql);
+$stmt->execute($parameters);
+$result = $stmt->fetch();
+
+$sqlTransactions = "SELECT * FROM transactions WHERE userId = :userId";
+$transParameters = array();
+$transParameters[':userId'] = $_SESSION['userId'];
+$transStmt = $dbConn->prepare($sqlTransactions);
+$transStmt->execute($transParameters);
+$transResults = $transStmt->fetchAll();
+
+// ranking system
+$soldRank = 0;
+$sqlNumSold = "SELECT COUNT(*) as numSold, license  FROM commInfo GROUP BY license ORDER BY numSold DESC";
+$stmtNumSold = $dbConn->prepare($sqlNumSold);
+$stmtNumSold->execute();
+$resultNumSold = $stmtNumSold->fetchAll();
+
+foreach ($resultNumSold as $sold) {
+    $soldRank++;
+    if($sold['license'] == $licenseResult['license'])
+    {
+        break;
+    }
+    else if($soldRank == count($resultNumSold))
+    {
+        $soldRank = 0;
+    }
+
+}
+
+$volumeRank = 0;
+$sqlVolSold = "SELECT SUM(InitialGross) as volSold, license  FROM commInfo GROUP BY license ORDER BY volSold DESC";
+$stmtVolSold = $dbConn->prepare($sqlVolSold);
+$stmtVolSold->execute();
+$resultVolSold = $stmtVolSold->fetchAll();
+foreach ($resultVolSold as $volume) {
+    $volumeRank++;
+    if($volume['license'] == $licenseResult['license'])
+    {
+        break;
+    }
+   else if($volumeRank == count($resultVolSold))
+    {
+        $volumeRank = 0;
+    }
+}
+
+$grossRank = 0;
+$sqlGrossRank = "SELECT SUM(finalComm) as gross, license  FROM commInfo GROUP BY license ORDER BY gross DESC";
+$stmtGross = $dbConn->prepare($sqlGrossRank);
+$stmtGross->execute();
+$resultGross = $stmtGross->fetchAll();
+foreach ($resultGross as $gross) {
+    $grossRank++;
+    if($gross['license'] == $licenseResult['license'])
+    {
+        break;
+    }
+   else if($grossRank == count($resultGross))
+    {
+        $grossRank = 0;
+    }
+}
+
+?>
+
+
+
 <!-- Main Header -->
 <header class="main-header">
     <!-- Header Navbar -->
@@ -6,130 +152,59 @@
         <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
+
+        <!-- Temporary button for easy switching -->
+        <?php
+            if ($_SESSION['userId'] == "100" || $_SESSION['userId'] == "34") //Jorge
+            {
+                $_SESSION['userId'] = "34";
+                $_SESSION['userType'] = "1";
+                echo "<a href=\"../../index.php\" style=\"margin-top: 8px; margin-left: 10px\" class=\"btn
+                btn-admin\">Switch to Admin</a>";
+            }
+            else if($_SESSION['userId'] == "101" || $_SESSION['userId'] == "37" ) //Juan
+            {
+                $_SESSION['userId'] = "37";
+                $_SESSION['userType'] = "1";
+                echo "<a href=\"../../index.php\" style=\"margin-top: 8px; margin-left: 10px\" class=\"btn
+                btn-admin\">Switch to Admin</a>";
+            }
+        ?>
+
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <!-- Messages: style is in dropdown.less-->
-                <li class="dropdown messages-menu">
-                    <!-- Menu toggle button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-envelope-o"></i> <span
-                                class="label label-success">4</span> </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">You have 4 messages</li>
-                        <li>
-                            <!-- Inner message menu -->
-                            <ul class="menu">
-                                <li>
-                                    <!-- Start message -->
-                                    <a href="#">
-                                        <div class="pull-left">
-                                            <!-- User Image --><img src=<?php echo "\"" . $agentInfo['picture'] . "\""; ?> 
-                                                                    class="img-circle" alt="User Image" style=""></div>
-                                        <!-- Message title and timestamp -->
-                                        <h4>
-                                            Support Team
-                                            <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                                        </h4>
-                                        <!-- Message content -->
-                                        <p>Why not buy a new awesome theme?</p>
-                                    </a>
-                                </li>
-                                <!-- End message -->
-                            </ul>
-                            <!-- /.menu -->
-                        </li>
-                        <li class="footer"><a href="#">See All Messages</a></li>
-                    </ul>
-                </li>
-                <!-- /.messages-menu -->
-                <!-- Notifications Menu -->
-                <li class="dropdown notifications-menu">
-                    <!-- Menu toggle button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-bell-o"></i> <span
-                                class="label label-warning">10</span> </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">You have 10 notifications</li>
-                        <li>
-                            <!-- Inner nofications menu -->
-                            <ul class="menu">
-                                <li>
-                                    <!-- Start notification -->
-                                    <a href="#"> <i class="fa fa-users text-aqua"></i> 5 new members joined today </a>
-                                </li>
-                                <!-- End notification -->
-                            </ul>
-                        </li>
-                        <li class="footer"><a href="#">View all</a></li>
-                    </ul>
-                </li>
-                <!-- Tasks Menu -->
-                <li class="dropdown tasks-menu">
-                    <!-- Menu Toggle Button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-flag-o"></i> <span
-                                class="label label-danger">9</span> </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">You have 9 tasks</li>
-                        <li>
-                            <!-- Inner tasks menu -->
-                            <ul class="menu">
-                                <li>
-                                    <!-- Task item -->
-                                    <a href="#">
-                                        <!-- Task title and progress text -->
-                                        <h3>
-                                            Design some buttons
-                                            <small class="pull-right">20%</small>
-                                        </h3>
-                                        <!-- Progress bar -->
-                                        <div class="progress xs">
-                                            <div class="progress-bar progress-bar-aqua" style="width: 20%"
-                                                 role="progressbar" aria-valuenow="20" aria-valuemin="0"
-                                                 aria-valuemax="100"><span class="sr-only">20% Complete</span></div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <!-- End task item -->
-                            </ul>
-                        </li>
-                        <li class="footer"><a href="#">View all tasks</a></li>
-                    </ul>
-                </li>
                 <!-- User Account Menu -->
                 <li class="dropdown user user-menu">
                     <!-- Menu Toggle Button -->
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <!-- User image in navbar--><img src="../../dist/img/user2-160x160.jpg" class="user-image"
-                                                         alt="User Image"> <span class="hidden-xs">Agent</span> </a>
+                        <!-- User image in navbar--><img src=<?php if(isset($agentInfo['picture']))
+                                                                    echo "agentPictures/". $agentInfo['picture'];
+                                                                else
+                                                                    echo "\"../dist/img/user2-160x160.jpg\""; ?> class="user-image"
+                                                         alt="User Image"> <span class="hidden-xs"><?php echo $agentInfo['firstName'] . " " . $agentInfo['lastName'] . " #" . $agentInfo['license'];?></span> </a>
                     <ul class="dropdown-menu">
                         <!-- User image in the menu -->
-                        <li class="user-header"><img src=<?php if(!isset($agentInfo['picture']))
-                                                                    echo "\"". $agentInfo['picture'] . "\""; 
+                        <li class="user-header"><img src=<?php if(isset($agentInfo['picture']))
+                                                                    echo "agentPictures/". $agentInfo['picture'];
                                                                 else
-                                                                    echo "../../dist/img/user2-160x160.jpg"; ?>  class="img-circle"
+                                                                    echo "\"../dist/img/user2-160x160.jpg\""; ?> class="img-circle"
                                                      alt="User Image">
-                            <p> Agent
+                            <p> <?=$_SESSION['username'];?>
                                 <small>Member since Nov. 2012</small>
                             </p>
                         </li>
-                        <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="row">
-                                <div class="col-xs-4 text-center"><a href="#">Followers</a></div>
-                                <div class="col-xs-4 text-center"><a href="#">Sales</a></div>
-                                <div class="col-xs-4 text-center"><a href="#">Friends</a></div>
-                            </div>
-                            <!-- /.row -->
-                        </li>
+
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <div class="pull-left"><a href="#" class="btn btn-default btn-flat">Profile</a></div>
+                            <div class="pull-left"><a href="settings.php" class="btn btn-default btn-flat">Settings</a></div>
                             <div class="pull-right"><a href="../logout.php" class="btn btn-default btn-flat">Sign
                                     out</a></div>
                         </li>
                     </ul>
                 </li>
                 <!-- Control Sidebar Toggle Button -->
-                <li><a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a></li>
+                <!-- <li><a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a></li> -->
             </ul>
         </div>
     </nav>
