344e1df2d5bf6f17f4ec50bcb8cb84549a7ef134
erinengle21 <eengle@csumb.edi>
2018-04-25 17:38:21 -0700

    changing skins of create flyer page

--- agent/openhouse/templates-oh/nav.php
+++ agent/openhouse/templates-oh/nav.php
@@ -1,34 +1,63 @@
-<!-- Left side column. contains the logo and sidebar -->
+<!-- Sidebar -->
 <aside class="main-sidebar">
     <!-- Sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <a href="../index.php" class="logo">
             <!-- Logo -->
-            <img class="img-responsive" src="../../dist/img/remax-logo.png">
-        </a>
+            <img class="img-responsive" src="../../dist/img/remax-logo.png"> </a>
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu" data-widget="tree">
-            <!--            <li class="active"><a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>-->
-            <li><a href="../my-inventory.php"><i class="fa fa-home"></i> <span>Listings</span></a></li>
-            <!-- <li><a href="visitors.php"><i class="fa fa-male"></i> <span>My Visitors</span></a></li> -->
+            <li class="header">OVERVIEW</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li><a href="../index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+<!--
+            <li><a href="../agent/openhouse/listings-openhouse.php"><i class="fa fa-home"></i>
+                    <span>Open House</span></a></li>
+-->
+            <li><a href="../../agent/my-calendar.php"><i class="fa fa-calendar"></i>
+                    <span>My Appointments</span></a></li>
+
+             <li><a href="../../agent/visitors.php"><i class="fa fa-address-book-o"></i>
+                    <span>My Visitors/Leads</span></a></li>
 
             <li class="header-style treeview">
-                <a href="visitors.php"><span class="header-color">My Visitors</span>
+                <a href="#"><span class="header-color">PROPERTIES</span>
                     <span class="pull-right-container">
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="visitors.php"><i class="fa fa-male"></i> <span>My Visitors</span></a></li>
-                    <li><a href="#"><i class="fa fa-list-alt"></i> <span id="exportVisitors">Export Visitors</span></a>
+                    <li><a class="dropdown-text" href="../my-inventory.php"><i class="fa fa-home"></i>
+                            <span>My Inventory</span></a></li>
+                    <li><a class="dropdown-text" href="../office-inventory.php"><i class="fa fa-building"></i> <span>Office Inventory</span></a>
+                    </li>
+                    <li><a class="dropdown-text" href="../inputNewListing.php"><i class="fa fa-upload"></i> <span>Input New  Listing</span></a>
                     </li>
                 </ul>
             </li>
-
-
-            <?php echo '<li><a href=../signIn.php?id=' . $listingId  . ' target="_blank"><i class="fa fa-edit"></i> <span>Sign In Sheet</span></a></li>'; ?>
-            <li><a href="#"><i class="fa fa-globe"></i> <span>Geofencing</span></a></li>
-            <li><a href="../index.php"><i class="fa fa-arrow-left"></i> <span>Return to Agent Dashboard</span></a></li>
+            <!-- <li class="header-style treeview">
+                <a href="#"><span class="header-color">TRANSACTIONS</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                      </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="salesBreakdown.php"><i class="fa fa-list-alt"></i> <span>Sales Breakdown</span></a></li>
+                    <li><a href="monthlyReport.php"><i class="fa fa-file-text-o"></i> <span>Monthly Report</span></a></li>
+                    <li><a href="pastSales.php"><i class="fa fa-archive"></i> <span>Past Sales</span></a></li>
+                </ul>
+            </li> -->
+            <!-- <li class="header-style treeview">
+                <a href="#"><span class="header-color">STATISTICS</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                      </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="analytics.php"><i class="fa fa-line-chart"></i> <span> Analytics</span></a></li>
+                    <li><a href="myEarnings.php"><i class="fa fa-money"></i> <span>My Earnings</span></a></li>
+                </ul>
+            </li> -->
         </ul>
         <!-- /.sidebar-menu -->
     </section>
